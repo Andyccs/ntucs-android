6794ae0ccfd4823c9a6f6d030d277319019264f7
Andy Chong Chin Shin <andychong2@gmail.com>
2016-05-20 14:29:47 +0800

    finally get a list view for main page

--- app/src/main/java/com/andyccs/ntucsrepo/MainFragment.java
+++ app/src/main/java/com/andyccs/ntucsrepo/MainFragment.java
@@ -5,11 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainFragment extends Fragment {
+
+  private static final String[] RESOURCE_LIST = new String[] {
+      "Course Projects",
+      "Final Year Project",
+      "PYP Solutions",
+      "Summaries",
+  };
+  private ListView resourceList;
 
   public MainFragment() {
   }
@@ -17,6 +27,17 @@ public class MainFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_main, container, false);
+    View mainView = inflater.inflate(R.layout.fragment_main, container, false);
+    resourceList = (ListView) mainView.findViewById(R.id.resource_list);
+
+    ArrayAdapter<String> resourceListAdapter = new ArrayAdapter<String>(
+        getActivity(),
+        R.layout.resource_text,
+        R.id.resource_text_1,
+        RESOURCE_LIST);
+    resourceList.setAdapter(resourceListAdapter);
+
+    return mainView;
   }
+
 }
